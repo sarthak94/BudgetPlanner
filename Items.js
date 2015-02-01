42272703bb83946deb030408c94ac2b995465a04
sarthak94 <sarthak@haribhakti.com>
2015-02-01 09:55:04 -0500

    dasd

--- Items.js
+++ Items.js
@@ -3,7 +3,7 @@
 
 var SixDollarItems = ["Thai Bowl","Mexican burrito","Greek Bowl"];
 var OneDollarItems = ["abc","def","fsdf","sfsd","fsfdsfsd","fsdfsd","fsdf","eyteryr","fsfgetetet","f"];
-
+ OneDollarItems.push("random");
  var itemGenerator = function(price){
  	var randomItem = Math.floor((Math.random() * 10) + 1);
  	if(price === 1){
@@ -11,9 +11,15 @@ var OneDollarItems = ["abc","def","fsdf","sfsd","fsfdsfsd","fsdfsd","fsdf","eyte
  		return 
  	}
  	if(price === 6){
- 		console.log(SixDollarItems[0]);
+ 		console.log(SixDollarItems[randomItem]);
  	}
  };
 
- itemGenerator(1);
- itemGenerator(6);
+ // itemGenerator(1);
+ // itemGenerator(6);
+var a;
+ for(var a = 0 ; a< OneDollarItems.length ; a++){
+ 	console.log(a.toString());
+ };
+
+ OneDollarItems.toString();
