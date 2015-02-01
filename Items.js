16664a0bc972e7b941ce5668d7e455448e82241d
sarthak94 <sarthak@haribhakti.com>
2015-01-31 20:36:51 -0500

    Random functions and arrays

--- Items.js
+++ Items.js
@@ -1,4 +1,19 @@
  
 
 
- var itemGenerator 
+var SixDollarItems = ["Thai Bowl","Mexican burrito","Greek Bowl"];
+var OneDollarItems = ["abc","def","fsdf","sfsd","fsfdsfsd","fsdfsd","fsdf","eyteryr","fsfgetetet","f"];
+
+ var itemGenerator = function(price){
+ 	var randomItem = Math.floor((Math.random() * 10) + 1);
+ 	if(price === 1){
+ 		console.log(OneDollarItems[randomItem]);
+ 		return 
+ 	}
+ 	if(price === 6){
+ 		console.log(SixDollarItems[0]);
+ 	}
+ };
+
+ itemGenerator(1);
+ itemGenerator(6);
